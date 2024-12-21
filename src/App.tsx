5828fdc961c9d3255bd5caf04c706396edd1743e
Thangrangto <thangrangto@192.168.2.96>
2024-12-21 15:59:28 +0700

    Lint staged testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import { RouterProvider } from 'react-router-dom';
 import { router } from './router';
 
-      const App = () => {
+const App = () => {
+  console.log('hello');
+
   return <RouterProvider router={router} />;
 };
 
