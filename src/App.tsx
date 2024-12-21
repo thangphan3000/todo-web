5b053b44a153ea86a293b4e650eebd5ab6e35ecc
Thangrangto <thangrangto@192.168.2.96>
2024-12-21 15:46:36 +0700

    Add new console content

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { RouterProvider } from 'react-router-dom';
 import { router } from './router';
 
 const App = () => {
-  console.log('hi');
+  console.log('hi there');
 
   return <RouterProvider router={router} />;
 };
