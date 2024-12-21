7b64b5389a52e9fad3969e23449211d6a9849725
Thangrangto <thangrangto@192.168.2.96>
2024-12-21 15:33:40 +0700

    Test husky

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
-import { RouterProvider } from "react-router-dom";
-import { router } from "./router";
+import { RouterProvider } from 'react-router-dom';
+import { router } from './router';
 
-                  function App() {
-                  return <RouterProvider router={router} />;
-}
+const App = () => {
+  console.log('hello');
+
+  return <RouterProvider router={router} />;
+};
 
 export default App;
